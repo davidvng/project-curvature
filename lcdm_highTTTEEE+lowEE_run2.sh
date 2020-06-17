2030f07daf2d9bda38aeddfd37a7dd9d83751451
sarnaaik <sarnaaik@discovery.hpc.usc.edu>
2020-06-17 13:32:57 -0700

    initial

---
+++ lcdm_highTTTEEE+lowEE_run2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --ntasks=6
+#SBATCH --cpus-per-task=8
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=3
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=48:00:00
+#SBATCH --mail-user=sarnaaik@usc.edu
+#SBATCH --mail-type=ALL
+#SBATCH --output=lcdm_highTTTEEE+lowEE_run2.log
+
+srun cobaya-run chains/lcdm_highTTTEEE+lowEE
