beb35ee5770ef1c0c0aa6facedd1386f908c7fd9
davidvng <davidvng@discovery.hpc.usc.edu>
2020-06-26 17:12:10 -0700

    initial

---
+++ lcdm_lmin800_chain2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=davidvng@usc.edu
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=12
+#SBATCH --nodes=2
+#SBATCH --mem-per-cpu=2GB
+#SBATCH --time=48:00:00
+#SBATCH --output=lcdm_lmin800_chain2.log
+
+srun python3 lcdm_lmin800.py
